ba0b1f95706a9d9ad813bc2144ec7772c2157645
JoeGilkey <jgilkey@gmail.com>
2014-10-28 11:28:43 -0400

    Added using options for jupload

--- src/jupload.c
+++ src/jupload.c
@@ -7,6 +7,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <curl/curl.h>
+#include <getopt.h>
+
+#include "version.h"
+
+char usage[] =
+
+"\n"
+"  %s\n"
+"\n"
+"  usage: jupload <options>\n"
+"\n"
+"  Options:\n"
+"      --dir           <path> : Path to dump files\n"
+"      -d                     : same as --dir \n"
+"      --filter <file filter> : Dump file filter\n"
+"      -f                     : same as --filter\n"
+"      --url            <url> : URL to upload the files to\n"
+"      -u                     : same as --url\n"
+"      --sleep      <seconds> : Number of seconds to sleep between checks\n"
+"      -s                     : same as --sleep\n"
+"\n"
+"      --help                : Displays this usage screen\n"
+"\n";
 
 void uploadFile(char *strFileName, char* uploadUrl, char expectNoHeader)
 {
@@ -58,17 +81,10 @@ void doProcessingLoop(char *dirName, char *fileFilter, char *uploadUrl)
 {
 	struct dirent **namelist;
 	int n;
-	if ( chdir(dirName) < 0 )
-	{
-		printf("Unable to set directory to %s\n", dirName);
-		perror("chdir");
-		return;
-	}
-	n = scandir(".", &namelist, NULL, alphasort);
+	n = scandir(dirName, &namelist, NULL, alphasort);
 	if (n < 0)
 	{
 		printf("Unable to search for %s files in %s\n", fileFilter, dirName);
-		perror("scandir");
 	}
 	else {
 		while ( n-- ) {
@@ -84,24 +100,120 @@ void doProcessingLoop(char *dirName, char *fileFilter, char *uploadUrl)
 
 int main(int argc, char *argv[])
 {
-	char* dirName;
-	char* fileFilter;
-	char* uploadUrl;
-	if(argc < 4) // not enough arguments!!!
-	{
-		printf("Incorrect number of arguments specified (%d expected, %d found)\n", 4, argc);
-		return 0;
-	}
+	static struct option long_options[] = {
+        {"dir",      1, 0, 'd'},
+        {"filter",   1, 0, 'f'},
+        {"url",      1, 0, 'u'},
+        {"sleep",    1, 0, 's'},
+        {"help",     0, 0, 'H'}
+    };
 
-	dirName = argv[1];
-	fileFilter = argv[2];
-	uploadUrl = argv[3];
+    int num_opts = 0;
+    int option = 0;
+    int option_index = 0;
+
+    int sleep_seconds = 5 * 60;
+	char* dirName = NULL;
+	char* fileFilter = NULL;
+	char* uploadUrl = NULL;
+	
+	/* check the arguments */
+
+    for(i=0; long_options[i].name != NULL; i++);
+    num_opts = i;
+
+    for(i=0; i<argc; i++) //go through all arguments
+    {
+        found = 0;
+        if(strlen(argv[i]) >= 3)
+        {
+            if(argv[i][0] == '-' && argv[i][1] != '-')
+            {
+                //we got a single dash followed by at least 2 chars
+                //lets check that against our long options to find errors
+                for(j=0; j<num_opts;j++)
+                {
+                    if( strcmp(argv[i]+1, long_options[j].name) == 0 )
+                    {
+                        //found long option after single dash
+                        found = 1;
+                        if(i>1 && strcmp(argv[i-1], "-") == 0)
+                        {
+                            //separated dashes?
+                            printf("Notice: You specified \"%s %s\". Did you mean \"%s%s\" instead?\n", argv[i-1], argv[i], argv[i-1], argv[i]);
+                        }
+                        else
+                        {
+                            //forgot second dash?
+                            printf("Notice: You specified \"%s\". Did you mean \"-%s\" instead?\n", argv[i], argv[i]);
+                        }
+                        break;
+                    }
+                }
+                if(found)
+                {
+                    sleep(3);
+                    break;
+                }
+            }
+        }
+    }
+
+    do
+    {
+    	option_index = 0;
+
+        option = getopt_long( argc, argv,
+                        "d:f:u:s:H",
+                        long_options, &option_index );
+
+        if( option < 0 ) break;
+
+        switch ( option )
+        {
+        	case 0 : break;
+        	case ':': printf("\"%s --help\" for help.\n", argv[0]); return( 1 );
+        	case '?': printf("\"%s --help\" for help.\n", argv[0]); return( 1 );
+        	case 'H': printf( usage, getVersion("jupload", _MAJ, _MIN, _SUB_MIN, _REVISION, _BETA, _RC)  ); return( 1 );
+        	case 'd':
+        		if ( dirName != NULL ) {
+        			printf("Notice: directory already given\n");
+        			break;
+        		}
+        		dirName = optarg;
+        		break;
+        	case 'f':
+        		if ( fileFilter != NULL ) {
+        			printf("Notice: filter already given\n");
+        			break;
+        		}
+        		fileFilter = optarg;
+        		break;
+        	case 'u':
+                if (uploadUrl != NULL) {
+                    printf( "Notice: upload url already given\n" );
+                    break;
+                }
+                uploadUrl = optarg;
+                break;
+            case 's':
+                sleep_seconds = atoi(optarg);
+                break;
+        }
+
+    } while(1);
+
+    if (fileFilter == NULL || dirName == NULL || uploadUrl == NULL)
+    	goto usage;
 
 	while(1)
 	{
 		doProcessingLoop(dirName, fileFilter, uploadUrl);
-		sleep( 60 * 5 );
+		sleep( sleep_seconds );
 	}
+
+usage:
+	printf( usage, getVersion("jupload", _MAJ, _MIN, _SUB_MIN, _REVISION, _BETA, _RC)  );
 
 	return 0;
 }
